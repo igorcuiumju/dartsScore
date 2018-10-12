c3dadbf4b70f1e9bd2c7da6dc7bbd5c52072b2e4
EBS\igor.cuiumju <igori1992laky@gmail.com>
2018-10-12 16:32:24 +0300

    first page js logic

--- app/js/controllers/dartsscore.controller.js
+++ app/js/controllers/dartsscore.controller.js
@@ -1,7 +1,31 @@
-function dartsscoreCtrl() {
-  var vm = this;
-  vm.test = 'Hello'
-}
+var app = angular.module("dartsScore", []);
 
-angular.module('dartsScore')
-.controller('dartsscoreCtrl', [dartsscoreCtrl])
+app.controller("firstCtrl", function($scope, $rootScope) {
+  $scope.player1;
+  $scope.player2;
+  $scope.selectgame;
+  $scope.selectrounds;
+  $scope.submit = function() {
+    $rootScope.$broadcast('myEvent', {
+      myPlayer1: $scope.player1,
+      myPlayer2: $scope.player2,
+      selectGame: $scope.selectgame,
+      selectRounds :$scope.selectrounds
+    })
+  }
+});
+
+app.controller("secondCtrl", function($scope) {
+  $scope.myPlayer1;
+  $scope.myPlayer2;
+  $scope.selectGame;
+  $scope.selectRounds;
+
+  $scope.$on('myEvent', function(event, message) {
+    $scope.gamesettings = true;
+    $scope.myPlayer1 = message.myPlayer1;
+    $scope.myPlayer2 = message.myPlayer2;
+    $scope.selectGame = message.selectGame;
+    $scope.selectRounds = message.selectRounds;
+  })
+});
